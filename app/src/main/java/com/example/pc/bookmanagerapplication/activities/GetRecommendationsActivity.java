325216f5f592c2ea18910401c73b81676cae39e9
Mihail Iliev <mihaililiev13@gmail.com>
2018-08-09 00:15:42 +0300

    added functionality to GetRecommendationsActivity

--- app/src/main/java/com/example/pc/bookmanagerapplication/activities/GetRecommendationsActivity.java
+++ app/src/main/java/com/example/pc/bookmanagerapplication/activities/GetRecommendationsActivity.java
@@ -1,16 +1,26 @@
 package com.example.pc.bookmanagerapplication.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 import com.example.pc.bookmanagerapplication.R;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GetRecommendationsActivity extends BaseDrawerActivity{
 
     public static final long ID = 4;
+    List<String> mSelection;
     Toolbar mToolbar;
-
-
+    Button mButton;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +29,17 @@ public class GetRecommendationsActivity extends BaseDrawerActivity{
         mToolbar = findViewById(R.id.tb_menu);
         setSupportActionBar(mToolbar);
 
+        mSelection = new ArrayList<>();
+        mButton = (Button) findViewById(R.id.bt_get_recommendations);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(GetRecommendationsActivity.this, CurrentlyReadingActivity.class);
+                intent.putExtra("SELECTED_GENRES", (Serializable) mSelection);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -29,5 +50,18 @@ public class GetRecommendationsActivity extends BaseDrawerActivity{
     @Override
     public long getIdentifier() {
         return ID;
+    }
+
+    public void selectItem(View view) {
+        CheckBox mCheckBox = (CheckBox) view;
+        String mCheckBoxText = String.valueOf(mCheckBox.getText());
+
+        if (mCheckBox.isChecked()) {
+            mSelection.add(mCheckBoxText);
+        } else {
+            mSelection.remove(mCheckBoxText);
+        }
+
+
     }
 }
