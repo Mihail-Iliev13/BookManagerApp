e0da0398bc7deca8d72fec3000f6743580b27dfe
Mihail Iliev <mihaililiev13@gmail.com>
2018-08-14 10:28:05 +0300

    added some more files

---
+++ app/src/main/java/com/example/pc/bookmanagerapplication/repository/base/Repository.java
@@ -0,0 +1,21 @@
+package com.example.pc.bookmanagerapplication.activities.repository.base;
+
+import android.content.Context;
+
+import com.example.pc.bookmanagerapplication.activities.models.Book;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public interface Repository<T> {
+
+    void getAll(Consumer<List<T>> action);
+
+    void add (Book book);
+
+//    boolean contains(Book book);
+
+    void remove (Book book);
+
+    String getCollectionName();
+}
